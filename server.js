a4f9b85d98f34fae0f8c50457a49c06afd9daeb4
Jakub Kida <jakub@snapp.dk>
2021-09-07 23:46:47 +0200

    cors!!!

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+var corsOptions = {
+    origin: "https://stupefied-franklin-783e20.netlify.app/"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
