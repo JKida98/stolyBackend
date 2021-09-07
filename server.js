445c533b4b375026190370c4250e6231eee3cd4b
Jakub Kida <jakub@snapp.dk>
2021-09-07 23:43:03 +0200

    fix error

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
-app.use(cors(corsOptions));
+});
 
 // parse requests of content-type - application/json
 app.use(express.json());
