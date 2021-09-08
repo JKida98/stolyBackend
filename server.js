fe592b506a822cb2203acca23ab2885ec32fbdd8
Jakub Kida <jakub@snapp.dk>
2021-09-08 17:27:54 +0200

    use cors??

--- server.js
+++ server.js
@@ -4,19 +4,7 @@ const cors = require("cors");
 const app = express();
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://stupefied-franklin-783e20.netlify.app/");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-    res.header("Access-Control-Allow-Credentials", true); 
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204);
-    }
-    next();
-  });
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
